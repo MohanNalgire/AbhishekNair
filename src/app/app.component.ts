77503939a5a42239b0a8cff990775cfefff2689e
mohan.nalgire <mnalgire@gmail.com>
2018-08-12 23:20:00 +0530

    Completed task 3h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Subscriber } from 'rxjs';
+import { FormGroup, FormControl, FormArray, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng6-project';
+  pageRoleMapping :Object;
+  pages   :Object;
+  roles   :Object;
+
+  constructor(private httpClient:HttpClient){}
+
+  ngOnInit(){
+    this.getPages();
+    this.getRoles();  
+  }
+
+  public getPages(){
+    return this.httpClient.get('http://localhost:3000/pages')
+    .subscribe(res=>this.pages=res);
+  }
+
+  public getRoles(){
+    return this.httpClient.get('http://localhost:3000/roles')
+    .subscribe(res=>this.roles=res);
+  }
+
+
+  public createPermission(myForm: NgForm){
+    //console.log('pages',myForm.value);
+    //console.log('pages',this.pages);
+    //console.log('roles',this.roles);
+    
+    let pageRoleMappingArray=[];
+    for (let [key, value] of Object.entries(this.roles)) {
+      let rolewiseAccess        =[];
+      rolewiseAccess['roleId']  =value.roleid;
+      rolewiseAccess['pagesIds']=[];
+      //console.log("roleid",value.roleid);
+            
+      for(let [key2,value2] of Object.entries(this.pages)){
+          let testaccess =value.roleid+''+value2.pageId;
+          if(myForm.value[testaccess]){
+            rolewiseAccess['pagesIds'].push(value2.pageId);      
+            //console.log(testaccess);
+          }
+      }
+      //console.log('rolewiseAccess',rolewiseAccess);
+      pageRoleMappingArray.push(rolewiseAccess);
+      }
+      //console.log('rolewiseAccess',pageRoleMapping);      
+      console.table(pageRoleMappingArray);
+       this.pageRoleMapping=pageRoleMappingArray;
+    }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
